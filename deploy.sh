ff8f33795724444eca3a3e99601ca0306339c636
Paulo Fagiani <pfagiani@gmail.com>
2022-04-14 21:29:52 +0000

    Fix tags and autoscaling

--- deploy.sh
+++ deploy.sh
@@ -65,9 +65,12 @@ if [ -z "$ECS_SERVICE_TASK_PROCESSES" ] || [[ $ECS_SERVICE_TASK_PROCESSES =~ $de
     release_arn=$(cat .releasearn)
 	provision_target_group_arn=$(cat .tgarn)
     deploy_desired_count=1
-	deploy_desired_count_regex="$deploy_process_type:([0-9]+).*"
+	deploy_autoscaling_policies="cpu=55"
+	deploy_desired_count_regex="$deploy_process_type[{};0-9]{0,}:([0-9]+)-{0,}([0-9]{0,})\[{0,}([;=0-9a-z]{0,})\]{0,}"
 	if [[ $ECS_SERVICE_TASK_PROCESSES =~ $deploy_desired_count_regex ]]; then
 		deploy_desired_count=${BASH_REMATCH[1]}
+		deploy_max_autoscaling_count=${BASH_REMATCH[2]}
+		deploy_autoscaling_policies=${BASH_REMATCH[3]}
 	fi
 
 	if [ -z "$PORT" ]; then
@@ -85,6 +88,7 @@ if [ -z "$ECS_SERVICE_TASK_PROCESSES" ] || [[ $ECS_SERVICE_TASK_PROCESSES =~ $de
 		--network-configuration "awsvpcConfiguration={subnets=[$ECS_SERVICE_SUBNETS],securityGroups=[$ECS_SERVICE_SECURITY_GROUPS]}" \
 		--desired-count $deploy_desired_count \
 		--enable-execute-command \
+		--propagate-tags TASK_DEFINITION \
 		--tags $deploy_json_workload_resource_tags \
         $( [ "$deploy_process_type" = "web" ] && echo "--load-balancers targetGroupArn=$provision_target_group_arn,containerName=$deploy_repository_slug,containerPort=$PORT")
 		)
@@ -96,9 +100,55 @@ if [ -z "$ECS_SERVICE_TASK_PROCESSES" ] || [[ $ECS_SERVICE_TASK_PROCESSES =~ $de
 		--task-definition $release_arn \
 		--network-configuration "awsvpcConfiguration={subnets=[$ECS_SERVICE_SUBNETS],securityGroups=[$ECS_SERVICE_SECURITY_GROUPS]}" \
 		--enable-execute-command \
+		--propagate-tags TASK_DEFINITION \
 		--force-new-deployment \
-		--desired-count $deploy_desired_count
+		$( [ -z "$deploy_max_autoscaling_count" ] && echo "--desired-count $deploy_desired_count")
 		)
 		echo "----> Rolling deployment of $release_arn with $deploy_desired_count task(s) in progress on ECS..."
+	fi
+
+	if [ ! -z "$deploy_max_autoscaling_count" ]; then
+		echo "----> Registering scalable target for $deploy_process_type"
+		aws application-autoscaling register-scalable-target \
+		--service-namespace ecs \
+		--resource-id service/$deploy_cluster_id/$deploy_service_name \
+		--scalable-dimension ecs:service:DesiredCount \
+		--min-capacity $deploy_desired_count \
+		--max-capacity $deploy_max_autoscaling_count
+
+		for policy in ${deploy_autoscaling_policies//;/ }; do
+			deploy_target_value=${policy#*=}
+			case $policy in
+				cpu=*)
+					deploy_predefined_metric_type="ECSServiceAverageCPUUtilization"
+					;;
+				mem=*)
+					deploy_predefined_metric_type="ECSServiceAverageMemoryUtilization"
+					;;
+				alb=*)
+					deploy_predefined_metric_type="ALBRequestCountPerTarget"
+					# TODO: discover ALB and TargetGroup ARNs
+					;;
+				*)
+					echo "Error: Unknown autoscaling policy $policy. Valid policies are: cpu=<value>, mem=<value>, alb=<value>"
+					exit 1
+					;;
+			esac
+			echo "----> Registering scaling policies for $deploy_process_type with $deploy_predefined_metric_type=$deploy_target_value"
+			aws application-autoscaling put-scaling-policy \
+			--service-namespace ecs \
+			--policy-name $deploy_service_name-cpu-scaling-policy \
+			--resource-id service/$deploy_cluster_id/$deploy_service_name \
+			--scalable-dimension ecs:service:DesiredCount \
+			--policy-type TargetTrackingScaling \
+			--target-tracking-scaling-policy-configuration "TargetValue=$deploy_target_value,PredefinedMetricSpecification={PredefinedMetricType=$deploy_predefined_metric_type}"
+		done
+	#else
+	 	# TODO: remove scalable target if it exists
+		#echo "----> Deregistering unnecessary scalable target for $deploy_process_type"
+		#aws application-autoscaling deregister-scalable-target \
+		#--service-namespace ecs \
+		#--scalable-dimension ecs:service:DesiredCount \
+		#--resource-id service/$deploy_cluster_id/$deploy_service_name
 	fi
 fi
