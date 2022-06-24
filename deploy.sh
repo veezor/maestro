e6f82497503278ec07fe16f9aad0bcf88810bba6
Paulo Fagiani <pfagiani@gmail.com>
2022-06-24 19:36:45 +0000

    Encapsulate command response

--- deploy.sh
+++ deploy.sh
@@ -159,13 +159,13 @@ if [[ $deploy_process_type != "scheduledtasks" && ( -z "$ECS_SERVICE_TASK_PROCES
 			deploy_current_scale_out_cooldown=$(echo $deploy_current_scaling_policy | jq --raw-output '.ScaleOutCooldown // 300')
 			deploy_current_scale_in_cooldown=$(echo $deploy_current_scaling_policy | jq --raw-output '.ScaleInCooldown // 300')
 			echo "----> Registering scaling policies for $deploy_process_type with $deploy_predefined_metric_type=$deploy_target_value"
-			aws application-autoscaling put-scaling-policy \
+			deploy_put_scaling_policy_return=$(aws application-autoscaling put-scaling-policy \
 			--service-namespace ecs \
 			--policy-name $deploy_service_name-$type_of-scaling-policy \
 			--resource-id service/$deploy_cluster_id/$deploy_service_name \
 			--scalable-dimension ecs:service:DesiredCount \
 			--policy-type TargetTrackingScaling \
-			--target-tracking-scaling-policy-configuration "TargetValue=$deploy_target_value,PredefinedMetricSpecification=$deploy_predefined_metric_specification,ScaleOutCooldown=$deploy_current_scale_out_cooldown,ScaleInCooldown=$deploy_current_scale_in_cooldown"
+			--target-tracking-scaling-policy-configuration "TargetValue=$deploy_target_value,PredefinedMetricSpecification=$deploy_predefined_metric_specification,ScaleOutCooldown=$deploy_current_scale_out_cooldown,ScaleInCooldown=$deploy_current_scale_in_cooldown")
 		done
 	#else
 	 	# TODO: remove scalable target if it exists
