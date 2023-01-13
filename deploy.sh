d96a7e68958a71a14b180b912e286f82628a1819
Matheus Ferreira <matheus@ip-172-31-79-151.ec2.internal>
2023-01-13 19:03:39 +0000

    Initial implementation of notification links

--- deploy.sh
+++ deploy.sh
@@ -164,7 +164,10 @@ if [[ $deploy_process_type != "scheduledtasks" && ( -z "$ECS_SERVICE_TASK_PROCES
                 deploy_webhook_parsed_url=${DEPLOY_WEBHOOK_URL/\{\{CLUSTER\}\}/$deploy_cluster_id}
                 deploy_webhook_parsed_url=${deploy_webhook_parsed_url/\{\{SERVICE\}\}/$deploy_service_name}
                 deploy_webhook_parsed_url=${deploy_webhook_parsed_url/\{\{REPOSITORY\}\}/$deploy_repository_slug}
-                deploy_webhook_response=$(curl -s -o /dev/null -w "%{http_code}" $deploy_webhook_parsed_url)
+		deploy_repo_link=$(aws codebuild batch-get-builds --ids $deploy_cluster_id-image-build:{{ID}}  --query 'builds[*].source.location')
+		deploy_codebuild_id=$(aws codebuild batch-get-builds --ids $deploy_cluster_id-image-build:{{ID}}  --query 'builds[*].serviceRole' | cut -d':' -f 5)
+		deploy_codebuild_link=$("https://us-east-1.console.aws.amazon.com/codesuite/codebuild/$deploy_codebuild_id/projects/$deploy_cluster_id/history?region=us-east-1")
+		deploy_webhook_response=$(curl -s -o /dev/null -w "%{http_code}" $deploy_webhook_parsed_url&$deploy_repo_link&$deploy_codebuild_link)
 
 		if test $deploy_webhook_response -ne 200; then
 			echo "    WARNING: Custom webhook deployment registration failed!"
