72552e235883460e72a8ebb243f65852cd193f8a
Matheus Ferreira <matheusdemelopf@gmail.com>
2023-04-14 20:08:19 +0000

    Add path and build-mode env variables

--- build.sh
+++ build.sh
@@ -31,12 +31,14 @@ if [ $(DOCKER_CLI_EXPERIMENTAL=enabled docker manifest inspect $build_image_name
 	exit 0
 fi
 
-if [ -f Dockerfile ]; then
+if [ ! -z $MAESTRO_BUILD_FOLDER ]; then
+	cd $MAESTRO_BUILD_FOLDER
+fi
+
+if [ $MAESTRO_BUILD_MODE == Dockerfile ]; then
         docker build -t ${build_image_name%:*}:latest -t $build_image_name .
 	build_built=true
-fi
-	
-if [ -f project.toml ]; then
+elif [ $MAESTRO_BUILD_FOLDER == project.toml ]; then
 	build_builder_name=`grep builder project.toml | cut -d= -f2 | tr -d '" '`
 	build_builder_tag=`echo $build_builder_name | tr /: -`
 	docker pull ${build_image_name%:*}:$build_builder_tag 2> /dev/null || true
