cd26798f9c8e3fe88bdfe34cc8720d76dde1058b
MatheusdeMelo <matheusdemelopf@gmail.com>
2025-06-26 14:07:29 +0100

    Add warning to AWS credentials

--- main.sh
+++ main.sh
@@ -57,6 +57,11 @@ main_application_environment_variables=$(jq -r '.SecretString | fromjson | to_en
 main_export_regex="^([A-Z0-9_])+[=]+(.*)"
 for ENV_LINE in $main_application_environment_variables; do
     if [[ $ENV_LINE =~ ${main_export_regex} ]]; then
+        key="${ENV_LINE%%=*}"
+        if [ $key == "AWS_ACCESS_KEY_ID" ] || [ $key == "AWS_SECRET_ACCESS_KEY" ]; then
+            echo "WARNING: the code need to be refactored to not use AWS credentials from secrets manager, but use the IAM role of the CodeBuild project instead."
+            exit 0
+        fi
         export $ENV_LINE
         echo $ENV_LINE >> .env
     else
