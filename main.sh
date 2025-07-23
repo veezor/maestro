3c0881e156071500c0295420ac6f9000c60ff918
MatheusdeMelo <matheusdemelopf@gmail.com>
2025-07-23 21:19:30 +0100

    change: add latest tag to build image

--- main.sh
+++ main.sh
@@ -89,7 +89,9 @@ else
     echo "----> Skipping build and running further steps..."
 fi
 
-main_processes=$(pack inspect $IMAGE_NAME | sed '0,/^Processes:$/d' | tail -n +2 | cut -d' ' -f3)
+LATEST_IMAGE_NAME=${IMAGE_NAME | cut -d ':' -f1}
+LATEST_IMAGE_NAME=$LATEST_IMAGE_NAME:latest
+main_processes=$(pack inspect $LATEST_IMAGE_NAME | sed '0,/^Processes:$/d' | tail -n +2 | cut -d' ' -f3)
 main_processes=${main_processes%$'\n'*}
 main_processes=${main_processes%$'\n'}
 # refactor line break below
