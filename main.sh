9751e4e07cf9033bcaa3631457ef1c2e2cb822f5
MatheusdeMelo <matheusdemelopf@gmail.com>
2025-07-25 20:42:00 +0100

    change: return to standard configuration

--- main.sh
+++ main.sh
@@ -89,8 +89,7 @@ else
     echo "----> Skipping build and running further steps..."
 fi
 
-LATEST_IMAGE_NAME="$(cut -d ':' -f1 <<< "$IMAGE_NAME"):latest"
-main_processes=$(pack inspect $LATEST_IMAGE_NAME | sed '0,/^Processes:$/d' | tail -n +2 | cut -d' ' -f3)
+main_processes=$(pack inspect $IMAGE_NAME | sed '0,/^Processes:$/d' | tail -n +2 | cut -d' ' -f3)
 main_processes=${main_processes%$'\n'*}
 main_processes=${main_processes%$'\n'}
 # refactor line break below
