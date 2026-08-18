8cd29e70bb464df7835da2da56da25b9aea9cfb8
renan veezor <renan.balbino56@gmail.com>
2026-08-18 10:22:15 -0300

    mend

--- deploy.sh
+++ deploy.sh
@@ -131,7 +131,7 @@ if [[ $deploy_process_type != "scheduledtasks" && ( -z "$ECS_SERVICE_TASK_PROCES
 		if [ ! -z "$deploy_json_workload_resource_tags" ]; then
 			aws ecs tag-resource \
 				--resource-arn "$deploy_service_arn" \
-				--tags "$deploy_json_workload_resource_tags"
+				--tags $deploy_json_workload_resource_tags
 		fi
 	fi
 
